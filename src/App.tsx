564836911b08c24118d0c5a8bae0e85828d0bfa2
Diego Paez <diego@geutstudio.com>
2026-02-27 21:40:32 +0000

    chore: Update App.tsx to use Layout component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Button } from "./components/button/button";
 import "./index.css";
+import { Layout } from "./components/layout/layout";
 
 
 export function App() {
   return (
-    <div className="container mx-auto p-8 text-center relative z-10">
-      <h1 className="text-2xl font-bold">Local Components</h1>
-      <p>Development View</p>
-      <div className="flex justify-center gap-4">
-        <Button className="rounded-full">Click me</Button>
+    <Layout maxWidth="lg" padding="none" centered={true} defaultTheme="light" storageKey="local-components-theme">
+      <div className="flex flex-col items-center justify-center mx-auto gap-4  text-center relative z-10 h-screen">
+        <h1 className="text-2xl font-bold -mt-10">Local Components</h1>
+        <p>Development View</p>
+        <div className="flex justify-center gap-4">
+          <Button className="rounded-full">Click me</Button>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
